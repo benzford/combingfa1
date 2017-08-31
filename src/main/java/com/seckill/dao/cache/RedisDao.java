f7b8f8d88399b3b79faf48bd4d647c020a198c24
mojo <mojo2016@163.com>
2017-09-01 05:46:08 +0800

    redis until debug faill 因为connection time out

--- src/main/java/com/seckill/dao/cache/RedisDao.java
+++ src/main/java/com/seckill/dao/cache/RedisDao.java
@@ -1,5 +1,8 @@
 package com.seckill.dao.cache;
 
+import com.dyuproject.protostuff.LinkedBuffer;
+import com.dyuproject.protostuff.ProtostuffIOUtil;
+import com.dyuproject.protostuff.runtime.RuntimeSchema;
 import com.seckill.entity.Seckill;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,36 +10,64 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 public class RedisDao {
-    private final Logger logger= LoggerFactory.getLogger(this.getClass());
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private JedisPool jedisPool;
 
     public RedisDao(String ip, int port) {
         jedisPool = new JedisPool(ip, port);
     }
 
+    private RuntimeSchema<Seckill> schema = RuntimeSchema.createFrom(Seckill.class);
+
     public Seckill getSeckill(long seckillId) {
         //redis操作逻逻辑
         try {
-            Jedis jedis=jedisPool.getResource();
+            Jedis jedis = jedisPool.getResource();
             try {
-                String key="seckill:"+seckillId;
+                String key = "seckill:" + seckillId;
                 //并没有实现内部序列化操作
                 //典型缓存的逻辑是这样的
                 //get ->byte() ->反序列化->Object(Seckill)
                 //java的各种序列化,google的protoBuffer性能最好->protoStuff
                 //很多序列化新能的指标: (1)序列化字节数最少
                 //采用自定义序列化
-
+                //protostuff:pojo
+                byte[] bytes = jedis.get(key.getBytes());
+                //缓存重新获取到
+                if (bytes != null) {
+                    //空对象
+                    Seckill seckill = schema.newMessage();
+                    ProtostuffIOUtil.mergeFrom(bytes, seckill, schema);
+//                    seckill反序列化
+                    return seckill;
+                }
             } finally {
                 jedis.close();
             }
-        } catch (Exception e){
-            logger.error(e.getMessage(),e);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
         }
         return null;
     }
 
-    public String putSeckillId(Seckill seckill) {
+    public String putSeckill(Seckill seckill) {
+        //set Object(seckill)->序列化->byte[]
+        try {
+            Jedis jedis = jedisPool.getResource();
+            try {
+                String key = "seckill:" + seckill.getSeckillId();
+                byte[] bytes = ProtostuffIOUtil.toByteArray(seckill, schema,
+                        LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE));
+                //超时缓存
+                int timeout=60*60;//1小时
+                String result = jedis.setex(key.getBytes(), timeout, bytes);
+                return result;
+            } finally {
+                jedis.close();
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
         return null;
     }
 }
